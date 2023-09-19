02bb6c73c36ed742a6bde122440083fe4cb0b565
Jhonata Costa <jhomcosta@gmail.com>
2023-09-19 10:52:13 -0300

    feat: api call implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,57 +3,61 @@ import { HomeContainer } from "./components/home";
 import { Product } from "./components/product";
 
 import camiseta1 from "../assets/camisetas/camisa1.png"
+import { stripe } from "@/lib/stripe";
+import Stripe from "stripe";
 
-export default function Home() {
+interface Product {
+  id: string;
+  name: string;
+  imageUrl: string;
+  price: number;
+}
+
+interface ProductsList {
+  products: Product[]
+}
+
+
+async function getProducts(): Promise<ProductsList> {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: price.unit_amount! / 100,
+    }
+  })
+
+  return { products }
+}
+
+
+export default async function Home() {
+  const { products } = await getProducts()
+
   return (
     <HomeContainer>
-      <Product>
-        <Image src={camiseta1} width={520} height={480} alt='camiseta' className="object-cover" />
+      {products.map(product => {
+        return (
+          <Product key={product.id}>
+            <Image src={product.imageUrl} width={520} height={480} alt='camiseta' className="object-cover" />
 
-        <footer
-          className="absolute bottom-1 left-1 right-1 p-8 rounded-md flex items-center justify-between bg-blackGradient
-          translate-y-[110%] opacity-0 transition-all ease-in-out delay-200 group-hover:translate-y-[0%] group-hover:opacity-100 "
-        >
-          <strong className="text-lg">Camiseta X</strong>
-          <span className="text-xl font-bold text-green300">R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product>
-        <Image src={camiseta1} width={520} height={480} alt='camiseta' className="object-cover" />
-
-        <footer
-          className="absolute bottom-1 left-1 right-1 p-8 rounded-md flex items-center justify-between bg-blackGradient
-          translate-y-[110%] opacity-0 transition-all ease-in-out delay-20 group-hover:translate-y-[0%] group-hover:opacity-100 "
-        >
-          <strong className="text-lg">Camiseta X</strong>
-          <span className="text-xl font-bold text-green300">R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product>
-        <Image src={camiseta1} width={520} height={480} alt='camiseta' className="object-cover" />
-
-        <footer
-          className="absolute bottom-1 left-1 right-1 p-8 rounded-md flex items-center justify-between bg-blackGradient
-          translate-y-[110%] opacity-0 transition-all ease-in-out delay-20 group-hover:translate-y-[0%] group-hover:opacity-100 "
-        >
-          <strong className="text-lg">Camiseta X</strong>
-          <span className="text-xl font-bold text-green300">R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product>
-        <Image src={camiseta1} width={520} height={480} alt='camiseta' className="object-cover" />
-
-        <footer
-          className="absolute bottom-1 left-1 right-1 p-8 rounded-md flex items-center justify-between bg-blackGradient
-          translate-y-[110%] opacity-0 transition-all ease-in-out delay-200 group-hover:translate-y-[0%] group-hover:opacity-100 "
-        >
-          <strong className="text-lg">Camiseta X</strong>
-          <span className="text-xl font-bold text-green300">R$ 79,90</span>
-        </footer>
-      </Product>
+            <footer
+              className="absolute bottom-1 left-1 right-1 p-8 rounded-md flex items-center justify-between bg-blackGradient
+            translate-y-[110%] opacity-0 transition-all ease-in-out delay-200 group-hover:translate-y-[0%] group-hover:opacity-100 "
+            >
+              <strong className="text-lg">{product.name}</strong>
+              <span className="text-xl font-bold text-green300">R$ {product.price}</span>
+            </footer>
+          </Product>
+        )
+      })}
     </HomeContainer>
   )
 }
