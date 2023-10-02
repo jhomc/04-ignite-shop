d7568655deda14fb56ac8ebcde9aa785396daedd
Jhonata Costa <jhomcosta@gmail.com>
2023-10-02 13:49:07 -0300

    deploy config

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
       },
     ],
   },
+
+  env: {
+    NEXT_URL: process.env.NEXT_URL,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY
+  }
 }
 
 module.exports = nextConfig
